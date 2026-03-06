9ed0bd9127b90f111967e0d376e269e808b7b5eb
oghenand <oghenand@bts-2x9qgk7-lt.ucsf.edu>
2026-03-06 12:25:12 -0800

    adding sequence code

--- nn/preprocess.py
+++ nn/preprocess.py
@@ -2,6 +2,8 @@
 import numpy as np
 from typing import List, Tuple
 from numpy.typing import ArrayLike
+# MY IMPORTS
+import random
 
 def sample_seqs(seqs: List[str], labels: List[bool]) -> Tuple[List[str], List[bool]]:
     """
@@ -20,7 +22,26 @@ def sample_seqs(seqs: List[str], labels: List[bool]) -> Tuple[List[str], List[bo
         sampled_labels: List[bool]
             List of labels for the sampled sequences
     """
-    pass
+    data_len = len(labels)
+    # first find minority class
+    minority_class = 0 if sum(labels) <=data_len/2 else 1
+    # find number to sample to balance
+    n_sample = sum([i for i in labels if i != minority_class]) - sum([i for i in labels if i == minority_class])
+    # get idxs to sample from - create set for faster (O(1)) look up
+    idx_sample = set(random.choices([i for i,j in enumerate(labels) if j == minority_class], k=n_sample))
+    # extract idx labels and sequences
+    sampled_labels = [label for i, label in enumerate(labels) if i in idx_sample]
+    sampled_seqs = [seq for i, seq in enumerate(seqs) if i in idx_sample]
+    # attach to original lists
+    sampled_seqs = seqs + sampled_seqs
+    sampled_labels = labels + sampled_labels
+    # shuffle since end of seqs/labels are all minority class
+    combined = list(zip(sampled_seqs, sampled_labels))
+    random.shuffle(combined)
+    sampled_seqs, sampled_labels = zip(*combined)
+    sampled_seqs, sampled_labels = list(sampled_seqs), list(sampled_labels)
+    # return
+    return sampled_seqs, sampled_labels
 
 def one_hot_encode_seqs(seq_arr: List[str]) -> ArrayLike:
     """
@@ -41,4 +62,19 @@ def one_hot_encode_seqs(seq_arr: List[str]) -> ArrayLike:
                 G -> [0, 0, 0, 1]
             Then, AGA -> [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0].
     """
-    pass
+    # initialize encoding based off what each base pair should match to
+    base_encode = {
+        'A': 0,
+        'T': 1,
+        'C': 2,
+        'G': 3,
+    }
+    encode_mapping = np.array([[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]])
+    # iterate w/ numpy
+    # first create list of encoding indices to extract (ie which element of encode_mapping to use)
+    # then use numpy to grab all the sequences in one shot
+    encodings = [
+        encode_mapping[[base_encode[base] for base in seq]].flatten() for seq in seq_arr
+    ]
+    encodings = np.array(encodings)
+    return encodings
